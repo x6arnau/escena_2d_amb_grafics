c1084fe967a8f858caa8af07e1f08446db3c29da
Arnau Nunez Lopez <arnaunl3@gmail.com>
2025-02-12 16:31:27 +0100

    Descanso

--- app/src/main/java/com/example/pintar/GraficNau.java
+++ app/src/main/java/com/example/pintar/GraficNau.java
@@ -1,5 +1,6 @@
 package com.example.pintar;
 
+import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.view.View;
 
@@ -7,5 +8,45 @@ public class GraficNau {
     private Drawable drawable;
     private View view;
 
-    private int ampleImg, altImg; //Imatge
+    private int ampleImg, altImg; //Imatge que dibuixarem
+    private double posX, posY; //Posició de la nau al Layout (vista)
+
+    //Constructor
+    public GraficNau(View view, Drawable drawable) {
+        this.view = view;
+        this.drawable = drawable;
+
+        //Agafem l'ample i l'alt de la imatge
+        ampleImg = drawable.getIntrinsicWidth();
+        altImg = drawable.getIntrinsicHeight();
+
+        //Posició de la nau a la vista inicial
+        this.posX = this.ampleImg + 5;
+        this.posY = this.altImg + 5;
+    }
+
+    //Mètode per dibuixar la nau
+    public void dibuixaGrafic(Canvas canvas) {
+        //definim el rectangle on posicionrem la nau
+        //obligatori definir setBounds
+        drawable.setBounds((int)posX, (int)posY, (int)posX + ampleImg, (int)posY + altImg);
+
+    }
+
+    //Getters i Setters
+    public double getPosY() {
+        return posY;
+    }
+
+    public void setPosY(double posY) {
+        this.posY = posY;
+    }
+
+    public double getPosX() {
+        return posX;
+    }
+
+    public void setPosX(double posX) {
+        this.posX = posX;
+    }
 }
--- app/src/main/java/com/example/pintar/MainActivity.java
+++ app/src/main/java/com/example/pintar/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pintar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
+import android.annotation.SuppressLint;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     Button b;
     ExempleView vistaExemple;
     /** Called when the activity is first created. */
+    @SuppressLint("MissingInflatedId")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,11 +22,23 @@ public class MainActivity extends AppCompatActivity {
         // Vista amb Layout
         setContentView(R.layout.activity_main);
 
-        //Pinto
+        // Vista directament de la classe
+        //setContentView(new ExempleView(this));
+
         vistaExemple = (ExempleView) this.findViewById(R.id.exview);
-        vistaExemple.generaShapes();
-        vistaExemple.postInvalidate();
-
-
+        b = (Button) findViewById(R.id.bt_change);
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                if (vistaExemple.cercleblau) {
+                    vistaExemple.cercleblau=false;
+                    vistaExemple.nau.setPosX(50);
+                } else {
+                    vistaExemple.cercleblau=true;
+                    vistaExemple.nau.setPosX(350);
+                }
+                vistaExemple.postInvalidate();
+            }
+        });
     }
 }
